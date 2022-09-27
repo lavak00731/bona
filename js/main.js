7fcf7a152c9e2c79d7236d4ff3961b585527bcb7
Ezequiel <eolavaca@gmail.com>
2022-09-27 13:53:34 -0300

    mobile ready

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function () {
+    function scrollHandling(e) {
+        const target = e.target.dataset.target;
+        const targetElem = document.querySelector('#'+target);
+        document.querySelectorAll('[data-remove="true"]').forEach((subsection) => subsection.setAttribute('hidden', true));
+            targetElem.removeAttribute('hidden');
+            targetElem.scrollIntoView({
+                behavior: "smooth"
+            });
+
+    }
+
+    document.querySelectorAll('button[data-target]').forEach((elem) => {
+        elem.addEventListener('click', scrollHandling);
+    });
+})();
